4ac6e33532524728f9bc60e164ce6dff6cf40802
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-12-03 02:21:24 +0300

    Exercise_1(Version02) completed

---
+++ Exercise_1(Version02)/Program.cs
@@ -0,0 +1,44 @@
+﻿int yourNumber = GetNumber("Enter your number ");
+int[] number = new int [5];
+FillArray(number, yourNumber);
+PrintArray(number);
+Console.WriteLine();
+PalindromeOrNot(number);
+
+
+void PalindromeOrNot(int[] number)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine("It's a palindome!");
+    }
+    else
+    Console.WriteLine("It's NOT a palindrome!");
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+    }
+}
+
+
+void FillArray(int[] array, int a)
+{
+    for (int i = array.Length - 1; i >= 0; i--)
+    {
+        int cutDigit = a % 10;
+        array[i] = cutDigit;
+        a = a / 10;
+    }
+}
+
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
